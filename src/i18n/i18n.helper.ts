746ffecf7e9ac4603d98147fff73a5ae8cf0cd93
Vladislav Mozharov <mozharovlad@gmail.com>
2024-04-18 16:23:12 +0300

    fix locale

--- src/i18n/i18n.helper.ts
+++ src/i18n/i18n.helper.ts
@@ -1,6 +1,7 @@
 import {I18n, TranslationVariables} from '@grammyjs/i18n'
+import path from 'path'
 
 export function translate(key: string, language = 'en', context?: TranslationVariables): string {
-  const i18n = new I18n({directory: 'src/i18n/locales'})
+  const i18n = new I18n({directory: path.resolve(__dirname, 'locales'), defaultLocale: 'en'})
   return i18n.t(language, key, context).replace(/[\u2068\u2069]/g, '')
 }
