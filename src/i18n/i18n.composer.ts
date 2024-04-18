1819fc13399c17e49079a534eaff60d865bf4dbb
Vladislav Mozharov <mozharovlad@gmail.com>
2024-04-18 15:20:06 +0300

    fix locales

--- src/i18n/i18n.composer.ts
+++ src/i18n/i18n.composer.ts
@@ -2,10 +2,11 @@ import {I18n} from '@grammyjs/i18n'
 import {Context} from '../context'
 import {Composer} from 'grammy'
 import {chatsService} from '../chats/chats.service'
+import path from 'path'
 
 export const i18n = new I18n<Context>({
   defaultLocale: 'en',
-  directory: 'src/i18n/locales',
+  directory: path.resolve(__dirname, 'locales'),
   localeNegotiator: async ctx => {
     const chat = ctx.chat?.id ? await chatsService.findChatByTelegramId(ctx.chat.id) : null
     return chat?.languageCode ?? ((ctx.chat?.type === 'private' && ctx.from?.language_code) || 'en')
