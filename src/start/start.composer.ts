f9f8487d31458efbfbb0384fc26aae03bd68480a
Vladislav Mozharov <mozharovlad@gmail.com>
2024-04-18 15:56:52 +0300

    remove commands start

--- src/start/start.composer.ts
+++ src/start/start.composer.ts
@@ -3,11 +3,14 @@ import {Context} from '../context'
 
 export const startComposer = new Composer<Context>()
 
-startComposer.chatType('private').command(['start', 'safe', 'restart'], async (ctx, next) => {
-  await ctx.conversation.exit()
-  await ctx.reply(ctx.t('start'), {parse_mode: 'HTML'})
-  await next()
-  await ctx.replyWithChatAction('typing')
-  await new Promise(resolve => setTimeout(resolve, 1500))
-  await ctx.reply(ctx.t('about-author'), {parse_mode: 'HTML'})
-})
+startComposer
+  .chatType('private')
+  .command('start')
+  .use(async (ctx, next) => {
+    await ctx.conversation.exit()
+    await ctx.reply(ctx.t('start'), {parse_mode: 'HTML'})
+    await next()
+    await ctx.replyWithChatAction('typing')
+    await new Promise(resolve => setTimeout(resolve, 1500))
+    await ctx.reply(ctx.t('about-author'), {parse_mode: 'HTML'})
+  })
