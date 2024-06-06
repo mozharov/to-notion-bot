00ab6b95d505fa32cfaf0684feaf7ce026f7e717
Vladislav Mozharov <mozharovlad@gmail.com>
2024-06-06 22:49:32 +0300

    deploy

--- src/typeorm/typeorm.migration.options.ts
+++ src/typeorm/typeorm.migration.options.ts
@@ -1,13 +1,10 @@
 import {DataSource} from 'typeorm'
 import typeormOptions from './typeorm.options'
-import {Migration1713375518739} from './migrations/1713375518739-migration'
-import {Migration1713442865423} from './migrations/1713442865423-migration'
-import {Migration1713526079330} from './migrations/1713526079330-migration'
 
 function buildTypeormMigrationConfig(): DataSource {
   return new DataSource({
     ...typeormOptions,
-    migrations: [Migration1713375518739, Migration1713442865423, Migration1713526079330],
+    migrations: [],
   })
 }
 
